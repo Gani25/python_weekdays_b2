e013fa7bde736b178b8a4b6952bc09caf21b2cd5
Gani25 <memonabdulgani3d@gmail.com>
2025-09-23 14:29:25 +0530

    oops

--- oops/cricket_player_module.py
+++ oops/cricket_player_module.py
@@ -1,13 +1,15 @@
+import datetime as dt
 class cricket_player:
 
     # Fields
     # constructor
-    def __init__(self,first_name = None, last_name = None, gender = None, team = None):
+    def __init__(self,first_name = None, last_name = None, gender = None, birth_year = None, team = None):
         # COnstructor is used to create object
         self.first_name = first_name
         self.last_name = last_name
         self.gender = gender
         self.team = team 
+        self.birth_year = birth_year
         self.scores = [] # Empty scores
         
 
@@ -16,3 +18,14 @@ class cricket_player:
 
     def add_scores(self, *scores):
         self.scores.extend(scores)
+
+    def sum_and_avg(self):
+        scores_sum = sum(self.scores)
+        avg_scores = scores_sum / len(self.scores)
+
+        return scores_sum,avg_scores
+    
+    # Age
+    def calculate_age(self):
+        current_year = dt.datetime.now().year
+        return current_year - self.birth_year
